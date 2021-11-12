bcef8ce6f0539e31877908afd6c0c0528a2bf1fc
Ivan Khoda <ivankhoda@gmail.com>
2021-11-12 23:02:39 +0300

    Heroku deployments preparations

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_112_093_358) do
+ActiveRecord::Schema.define(version: 2021_11_12_093358) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'projects', force: :cascade do |t|
-    t.string 'title'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'tasks', force: :cascade do |t|
-    t.string 'text'
-    t.boolean 'isCompleted'
-    t.bigint 'project_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['project_id'], name: 'index_tasks_on_project_id'
+  create_table "tasks", force: :cascade do |t|
+    t.string "text"
+    t.boolean "isCompleted"
+    t.bigint "project_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
-  add_foreign_key 'tasks', 'projects'
+  add_foreign_key "tasks", "projects"
 end
